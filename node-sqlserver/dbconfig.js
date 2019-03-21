8a2ca016ed2927898be13038a8866cfdbe7498c8
Andrew Syrmakesis <siarziss@gmail.com>
2019-03-21 17:14:20 +0200

    configuration file

---
+++ node-sqlserver/dbconfig.js
@@ -0,0 +1,15 @@
+const sql = require("mssql");
+
+// 'config' variable is a dictionary exported to server.js script within
+// the same directory for privacy & security reasons
+
+const config = {
+    user:  "****",
+    password: "***",
+	server: "***",
+	port: 0,
+    database: "***"
+}
+
+const pool = new sql.ConnectionPool(config);
+module.exports = pool;
